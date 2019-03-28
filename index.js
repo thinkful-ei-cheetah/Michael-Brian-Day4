bfe0f83357d2cc9902771376239d0110034e7cf4
Brian Sandoval <39751822+iambrianshin@users.noreply.github.com>
2019-03-28 14:54:19 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+"use strict";
+
+function handlecatClicks() {
+  $(".thumbnails").on("click", "img", function(event) {
+    event.stopPropagation();
+    const targetCat = $(this).attr("src");
+    const otherCat = $(this).attr("alt");
+
+    $(".hero img")
+      .attr("src", targetCat)
+      .attr("alt", otherCat);
+  });
+}
+
+$(handlecatClicks);
